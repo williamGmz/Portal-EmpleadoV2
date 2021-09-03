d22bac6fa757a33c91910aebf463dc7003ead60a
Fernando-Valdes <55217268+Fernando-Valdes@users.noreply.github.com>
2021-09-03 16:48:22 -0500

    definición de la barra de menu lateral
    de definió el primero borrador del menú lateral y head, así como footer.

---
+++ menu.php
@@ -0,0 +1,188 @@
+<body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Tribunal Administrativo</span></a>
+            </div>
+            <div class="clearfix"></div>
+
+            <!-- Foto, Nombre, Y bienvenido al empleado -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Bienvenido/a</span>
+                <h2>John Doe</h2>
+              </div>
+            </div>
+            <!-- Termino de: Foto, Nombre, Y bienvenido al empleado -->
+
+
+            <br />
+
+            <!-- Menu lateral-->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Mi perfil <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Datos personales</a></li>
+                      <li><a href="index2.html">Datos Laborales</a></li>
+                      <li><a href="index3.html">-------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Nóminas <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Ver Nóminas</a></li>
+                      <li><a href="form_advanced.html">----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Declaraciones <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Mis declaracioes</a></li>
+                      <li><a href="form_advanced.html">----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-desktop"></i> Resguardos <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="general_elements.html">Equipos informáticos</a></li>
+                      <li><a href="media_gallery.html">Mobiliario</a></li>
+                      <li><a href="typography.html">----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-table"></i> Calendario <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="chartjs.html">Avisos</a></li>
+                      <li><a href="echarts.html">-----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-bar-chart-o"></i> Ayuda y soporte técnico <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="chartjs2.html">Sugerencias del sistema</a></li>
+                      <li><a href="morisjs.html">Reportar un error</a></li>
+                      <li><a href="echarts.html">Solicitar asistencias técnica</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Administrador</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-bug"></i> Usuarios <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="e_commerce.html">Todos los usuarios</a></li>
+                      <li><a href="projects.html">-------------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-windows"></i> Token <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="page_403.html">Registro portal empleado</a></li>
+                      <li><a href="page_404.html">Registro sistema de sentencias</a></li>
+                      <li><a href="page_500.html">----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-sitemap"></i> Informática <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="#level1_1">Ip eqipos informáticos</a>
+                        <li><a>Contraseñas<span class="fa fa-chevron-down"></span></a>
+                          <ul class="nav child_menu">
+                            <li class="sub_menu"><a href="level2.html">Control de contraseña</a>
+                            </li>
+                            <li><a href="#level2_1">--------</a>
+                            </li>
+                            <li><a href="#level2_2">--------</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li><a href="#level1_2">----------</a>
+                        </li>
+                    </ul>
+                    <li><a><i class="fa fa-windows"></i> Avisos <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="page_403.html">Lista de Avisos</a></li>
+                      <li><a href="page_404.html">Notificaciones</a></li>
+                      <li><a href="page_500.html">----------</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-windows"></i> Asistencia técnica <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="page_403.html">Reporte de errores</a></li>
+                      <li><a href="page_404.html">Sugerencias | Comentarios</a></li>
+                      <li><a href="page_500.html">Asistencia técnica</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+              </a>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+              <nav class="nav navbar-nav">
+              <ul class=" navbar-right">
+                <li class="nav-item dropdown open" style="padding-left: 15px;">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">John Doe
+                  </a>
+                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item"  href="javascript:;">Mi perfil</a>
+                    <a class="dropdown-item"  href="javascript:;">Cambiar contraseña</a>
+                  <a class="dropdown-item"  href="javascript:;"></a>
+                    <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Salir</a>
+                  </div>
+                </li>
+
+                <!-- Notificaciones -->
+                <li role="presentation" class="nav-item dropdown open">
+                  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>
+                  </a>
+                  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
