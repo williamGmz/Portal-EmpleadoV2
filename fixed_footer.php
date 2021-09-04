30f9930a36f18ebd0851dad288e67ff1278f06d6
williamGmz <87445646+williamGmz@users.noreply.github.com>
2021-09-04 12:51:08 -0500

    se agregó Header y footer en 7 archivos.

---
+++ fixed_footer.php
@@ -0,0 +1,19 @@
+<?php
+    include("head.php");
+    include("menu.php");
+?>
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Fixed Footer <small> Just add class <strong>footer_fixed</strong></small></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+<?php
+      include("footer.php");
+?>
