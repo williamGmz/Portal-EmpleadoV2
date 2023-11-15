66b411a0f298e39f2f9151eec75cfd1d90da3740
omao <60245388+omao-star@users.noreply.github.com>
2023-11-15 12:40:44 -0600

    Se cambia ruta de index

--- view/Head/head.php
+++ view/Head/head.php
@@ -6,28 +6,28 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="public/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link href="././public/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+	<link rel="icon" href="././public/images/favicon.ico" type="image/ico" />
 
     <title>Gentelella Alela! | </title>
 
     <!-- Bootstrap -->
-    <link href="public/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="././public/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="public/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="././public/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
-    <link href="public/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="././public/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
-    <link href="public/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="././public/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
     <!-- jQuery custom content scroller -->
-    <link href="public/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css" rel="stylesheet"/>
+    <link href="././public/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css" rel="stylesheet"/>
     <!-- bootstrap-progressbar -->
-    <link href="public/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <link href="././public/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
     <!-- JQVMap -->
-    <link href="public/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <link href="././public/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
     <!-- bootstrap-daterangepicker -->
-    <link href="public/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="././public/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
     <!-- Custom Theme Style -->
-    <link href="public/build/css/custom.min.css" rel="stylesheet">
+    <link href="././public/build/css/custom.min.css" rel="stylesheet">
   </head>
